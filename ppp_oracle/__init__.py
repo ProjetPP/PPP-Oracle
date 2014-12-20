15026e0b807bd9c4102c0121a25301ef4f529cf5
Valentin Lorentz <progval@progval.net>
2014-12-20 21:31:16 +0100

    Add basic module, as created by the script.

---
+++ ppp_oracle/__init__.py
@@ -0,0 +1,9 @@
+"""A module that answers hardcoded questions."""
+
+from ppp_libmodule import HttpRequestHandler
+from .requesthandler import RequestHandler
+
+def app(environ, start_response):
+    """Function called by the WSGI server."""
+    return HttpRequestHandler(environ, start_response, RequestHandler) \
+            .dispatch()
